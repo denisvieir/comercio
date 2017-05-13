d72ab20736a6a469e198f4651bb1dd2cbdea59a5
Denis Vieira <denisvieira07@gmail.com>
2017-05-13 01:35:47 -0300

    Commit inicial

---
+++ ControladorDePedidos.MOdel/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControladorDePedidos.Model
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+
+        public Marca Marca { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal ValorDeCompra { get; set; }
+
+        public decimal ValorDeVenda { get; set; }
+
+        public int QuantidadeEmEstoque { get; set; }
+
+        public int QuantidadeMinimaEmEstoque { get; set; }
+    }
+}
---
+++ ControladorDePedidos.WPF/FormCadastroDeProduto.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ControladorDePedidos.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControladorDePedidos.WPF
+{
+    /// <summary>
+    /// Interaction logic for FormCadastroDeProduto.xaml
+    /// </summary>
+    public partial class FormCadastroDeProduto : Window
+    {
+        RepositorioMarca repositorio;
+        public FormCadastroDeProduto()
+        {
+            repositorio = new RepositorioMarca();
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var marcas = repositorio.Liste();
+            cmbMarcas.DataContext = marcas;
+        }
+    }
+}
